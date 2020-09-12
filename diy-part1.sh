86bd722a000e2c77fc6433de60bd9c71fd094dae
openwrt-1983 <openwrt-1983@qq.com>
2020-09-12 22:15:10 +0800

    ''

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,17 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 
-# git clone https://github.com/openwrt-1983/openwrt-branch package/long1983
+
+
+# src-git helloworld https://github.com/fw876/helloworld
+# src-git kenzo https://github.com/kenzok8/openwrt-packages
+# src-git small https://github.com/kenzok8/small
+# src-git mypackages https://github.com/siropboy/mypackages
+# src-git destan19 https://github.com/destan19/OpenAppFilter
+# src-git branch https://github.com/openwrt-1983/openwrt-branch
+
+
+
 
 git clone https://github.com/fw876/helloworld feeds/helloworld
 
@@ -27,7 +37,7 @@ git clone https://github.com/kenzok8/small package/small
 
 git clone https://github.com/siropboy/mypackages package/mypackages
 
-git clone https://github.com/tty228/luci-app-serverchan package/tty228
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 
 git clone https://github.com/destan19/OpenAppFilter package/destan19
 
@@ -36,17 +46,5 @@ git clone https://github.com/openwrt-1983/openwrt-branch package/branch
 # cp package/branch/patch/add-thermal-driver-for-H6H5H3A64A83TR40.patch target/linux/sunxi/patches-5.4/add-thermal-driver-for-H6H5H3A64A83TR40.patch
 # cp package/branch/patch/add-YAML-schema-for-sun8i-thermal-driver-bindings.patch target/linux/sunxi/patches-5.4/add-YAML-schema-for-sun8i-thermal-driver-bindings.patch
 # cp package/branch/patch/sun8i-h3-Add-thermal-sensor-and-thermal-zones.patch target/linux/sunxi/patches-5.4/sun8i-h3-Add-thermal-sensor-and-thermal-zones.patch
-
-
-
-
-
-
-
-
-
-
-
-
 
 
--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,11 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
+sed -i 's/tplink-4mlzma/tplink-16mlzma/g' target/linux/ar71xx/image/tiny-tp-link.mk
+
+sed -i 's/tplink-8mlzma/tplink-16mlzma/g' target/linux/ar71xx/image/generic-tp-link.mk
+
+
 
 
 
